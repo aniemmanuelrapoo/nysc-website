dee76eee859743290230581867f5f5ac596793f3
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-10-18 23:37:29 +0100

    service component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Services from './components/Services/Services';
 import TopBanner from './components/TopBanner/TopBanner';
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <TopBanner />
       <Services />
     </div>
--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import designIcon from '../../asset/image/design.png';
-import ecommIcon from '../../asset/image/ecommerce.png';
-import webIcon from '../../asset/image/web.png';
+import updateIcon from '../../asset/image/updated.png';
+import activitesIcon from '../../asset/image/mental-health.png';
+import eventIcon from '../../asset/image/event.png';
 import '../../asset/css/custom.css'
 import '../../asset/css/bootstrap.min.css'
 
@@ -12,25 +12,27 @@ interface IServicesProps {
 const Services: React.FunctionComponent<IServicesProps> = (props) => {
   return(
     <>
-      <Container>
+      <Container className='text-center'>
+        <h1 className='service__title'>OUR SERVICES</h1>
+        <div className="service__titleBottom"></div>
         <Row>
           <Col lg={4} md={6} sm={12}>
             <div className='service__card'>
-              <img className='service__img1' src={designIcon} alt="" />
+              <img className='service__img1' src={updateIcon} alt="" />
               <h2 className='service__name'>Recent Update</h2>
               <p className='service__discription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem</p>
             </div>
           </Col>
           <Col lg={4} md={6} sm={12}>
             <div className='service__card'>
-              <img className='service__img1' src={ecommIcon} alt="" />
+              <img className='service__img1' src={activitesIcon} alt="" />
               <h2 className='service__name'>All Camp Activites</h2>
               <p className='service__discription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem</p>
             </div>
           </Col>
           <Col lg={4} md={6} sm={12}>
             <div className='service__card'>
-              <img className='service__img1' src={webIcon} alt="" />
+              <img className='service__img1' src={eventIcon} alt="" />
               <h2 className='service__name'>Upcoming Events</h2>
               <p className='service__discription'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem</p>
             </div>
