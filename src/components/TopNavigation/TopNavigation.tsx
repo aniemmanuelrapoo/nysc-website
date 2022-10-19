e921843185775676b4c66aa7f60e183360cb62ab
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-10-19 06:22:04 +0100

    navbar section 2

--- src/components/TopNavigation/TopNavigation.tsx
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -5,11 +5,26 @@ interface ITopNavigationProps {
 }
 
 const TopNavigation: React.FunctionComponent<ITopNavigationProps> = (props) => {
+  const [navBarTitle, setNavBarTitle] = React.useState('nav__title')
+  
+  React.useEffect(() => {
+    const onScroll = () => {
+      if(window.scrollY > 100){
+        setNavBarTitle('nav__titleScroll')
+      }else if(window.scrollY < 100){
+        setNavBarTitle('nav__title')
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return() => window.removeEventListener('scroll', onScroll)
+  })
   return (
     <>
       <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">NYSC IN CAMP ACTIVITIES</Navbar.Brand>
+        <Navbar.Brand href="#home" className={navBarTitle}>NYSC IN CAMP ACTIVITIES</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
