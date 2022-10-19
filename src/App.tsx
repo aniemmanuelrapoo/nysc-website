63a333bb18f936a39d5abfd9ebf34d990ec6e75e
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-10-19 05:17:41 +0100

    navbar menu part1

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Services from './components/Services/Services';
 import TopBanner from './components/TopBanner/TopBanner';
 import './App.css'
+import TopNavigation from './components/TopNavigation/TopNavigation';
 
 function App() {
   return (
     <div className='App'>
+      <TopNavigation />
       <TopBanner />
       <Services />
     </div>
---
+++ src/components/TopNavigation/TopNavigation.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+interface ITopNavigationProps {
+}
+
+const TopNavigation: React.FunctionComponent<ITopNavigationProps> = (props) => {
+  return (
+    <>
+      <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">NYSC IN CAMP ACTIVITIES</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">HOME</Nav.Link>
+            <Nav.Link href="#deets">ABOUT</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#deets">SERVICES</Nav.Link>
+            <Nav.Link href="#deets">COURSES</Nav.Link>
+            <Nav.Link href="#deets">PORTFOLIO</Nav.Link>
+            <Nav.Link href="#deets">CONTACT US</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default TopNavigation;
