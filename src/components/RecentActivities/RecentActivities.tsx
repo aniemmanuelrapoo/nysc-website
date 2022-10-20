b02381ff6bb0469a1dd127a5243fd554212cf775
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-10-20 04:06:36 +0100

    activities section

--- src/components/RecentActivities/RecentActivities.tsx
+++ src/components/RecentActivities/RecentActivities.tsx
@@ -12,41 +12,41 @@ const RecentActivities: React.FunctionComponent<IRecentActivitiesProps> = (props
         <div className="service__titleBottom"></div>
         <Row className='text-dark'>
           <Col lg={4} md={6} sm={12}>
-            <Card style={{ width: '18rem' }}>
+            <Card className='activity__card'>
               <Card.Img variant="top" src="https://img.freepik.com/free-photo/book-with-green-board-background_1150-3837.jpg?w=740&t=st=1666230708~exp=1666231308~hmac=23ff5dc5321b7ab8c9e7582d900b657c7a7bd8a4a9b7a3e10bde584ee3f0ee21" />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
+                <Card.Title className='service__name'>Activity One</Card.Title>
+                <Card.Text className='service__discription'>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">View All Activities</Button>
               </Card.Body>
             </Card>
           </Col>
           <Col lg={4} md={6} sm={12}>
-            <Card style={{ width: '18rem' }}>
+            <Card className='activity__card'>
               <Card.Img variant="top" src="https://img.freepik.com/premium-photo/beautiful-young-african-woman-reading-book-library_219728-4340.jpg?w=740" />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
+                <Card.Title className='service__name'>Activity Two</Card.Title>
+                <Card.Text className='service__discription'>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">View All Activities</Button>
               </Card.Body>
             </Card>
           </Col>
           <Col lg={4} md={6} sm={12}>
-            <Card style={{ width: '18rem' }}>
+            <Card className='activity__card'>
               <Card.Img variant="top" src="https://img.freepik.com/free-photo/positive-college-student-has-dark-skin-carries-folders-book-points-with-cheerful-expression-aside-has-toothy-smile_273609-23704.jpg?w=740&t=st=1666230929~exp=1666231529~hmac=a117985d6e3473d1cdd532b2b58a705eb27a1a4cf56f172eccb9e1f5b62165f5" />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
+                <Card.Title className='service__name'>Activity Three</Card.Title>
+                <Card.Text className='service__discription'>
                   Some quick example text to build on the card title and make up the
                   bulk of the card's content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">View All Activities</Button>
               </Card.Body>
             </Card>
           </Col>
