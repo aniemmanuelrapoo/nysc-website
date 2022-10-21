36669c2032d8f2402a1983baa7c1f4859c11146c
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-10-21 18:08:24 +0100

    done with service page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import './App.css'
 import './asset/css/custom.css'
 import './asset/css/bootstrap.min.css'
-import ContactPage from './pages/ContactPage';
+import AllServicePage from './pages/AllServicePage';
 
 function App() {
   return (
     <div className='App'>
-      <ContactPage />
+      <AllServicePage />
     </div>
   );
 }
---
+++ src/pages/AllServicePage.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import ContactSec from '../components/ContactSec/ContactSec';
+import Footer from '../components/Footer/Footer';
+import PageTop from '../components/PageTop/PageTop';
+import Services from '../components/Services/Services';
+import TopNavigation from '../components/TopNavigation/TopNavigation';
+
+interface IAllServicePageProps {
+}
+
+const AllServicePage: React.FunctionComponent<IAllServicePageProps> = (props) => {
+  return (
+    <>
+      <TopNavigation />
+      <PageTop pageTitle="Our Services" />
+      <Services />
+      <ContactSec />
+      <Footer />
+    </>
+  );
+};
+
+export default AllServicePage;
