7c62e92debf03360b8d02b72dac2112fc8ceab9a
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-10-29 03:12:52 +0100

    contact api

--- src/components/ContactSec/ContactSec.tsx
+++ src/components/ContactSec/ContactSec.tsx
@@ -4,11 +4,49 @@ import * as React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { FooterData } from '../../RestApi/AppUrl';
+import axios from 'axios';
 
 interface IContactSecProps {
 }
 
+interface IPost {
+  id: number
+  address: string
+  email: string
+  phone: string
+}
+
+const defaultPosts:IPost[] = [];
+
 const ContactSec: React.FunctionComponent<IContactSecProps> = (props) => {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(defaultPosts);
+  const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
+  const [error, setError]: [string, (error: string) => void] = React.useState("");
+
+  React.useEffect(() => {
+      axios
+      .get<IPost[]>(`${FooterData}`, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          timeout : 60000
+        })
+      .then(response => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch(ex => {
+        const err =
+        ex.code === "ECONNABORTED"
+          ? "A timeout has occurred"
+          : ex.response.status === 404
+            ? "Resource not found"
+            : '';
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
   return(
     <>
       <Container className='mt-5'>
@@ -39,9 +77,9 @@ const ContactSec: React.FunctionComponent<IContactSecProps> = (props) => {
           <Col lg={6} md={6} s={12}>
             <h1 className='service__name'>Discuss Now</h1>
             <p className="service__discription">
-              NYSC Orentation Camp Sibry Taraba State <br></br>
-              <FontAwesomeIcon icon={faEnvelope}  /> Email : Support@gmail.com<br></br>
-              <FontAwesomeIcon icon={faPhone}  /> Phone : +2348036636819<br></br>
+            {loading ? 'loading' : error ? error : posts[0]['address']} <br></br>
+              <FontAwesomeIcon icon={faEnvelope}  /> Email : {loading ? 'loading' : error ? error : posts[0]['email']}<br></br>
+              <FontAwesomeIcon icon={faPhone}  /> Phone : {loading ? 'loading' : error ? error : posts[0]['phone']}<br></br>
           </p>
           </Col>
         </Row>
